444d234bbd9ba23546543732e8a92dcaf32a70e4
Simone Garuglieri <s.garuglieri@gmail.com>
2018-10-27 14:05:36 +0200

    Initial Commit

---
+++ User Parsing Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  User Parsing Test
+//
+//  Created by Simone Garuglieri on 27/10/2018.
+//  Copyright © 2018 Simone Garuglieri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ User Parsing TestTests/User_Parsing_TestTests.swift
@@ -0,0 +1,34 @@
+//
+//  User_Parsing_TestTests.swift
+//  User Parsing TestTests
+//
+//  Created by Simone Garuglieri on 27/10/2018.
+//  Copyright © 2018 Simone Garuglieri. All rights reserved.
+//
+
+import XCTest
+@testable import User_Parsing_Test
+
+class User_Parsing_TestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
